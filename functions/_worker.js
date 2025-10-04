ae2886b99e367f5932fefedcbe60b2397217e5b7
yamdeeyoumar <umer_1976@hotmail.com>
2025-10-04 14:50:54 -0400

    Remove unused _worker.js file
    Deleted the _worker.js file from the functions directory as it is no longer needed for API endpoint routing.

--- functions/_worker.js
+++
@@ -1,7 +0,0 @@
-// ✅ Auto-route all files in /functions/api/* as API endpoints
-import { createPagesFunctionHandler } from 'cloudflare:pages';
-
-// this tells Cloudflare to expose each file as an endpoint
-export default createPagesFunctionHandler({
-  directory: './',
-});
